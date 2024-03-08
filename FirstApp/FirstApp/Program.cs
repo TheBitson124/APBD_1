3692e6ed35e626fe6b5fa51a2363ebba8c0473f5
TheBitson124 <as.sosnowicz@gmail.com>
2024-03-08 15:17:36 +0100

    Add GetAverage method

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -4,3 +4,13 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World! AGAIN");
 Console.WriteLine("Hello, World! AGAIN AND AGAIN");
 Console.WriteLine("bruh");
+
+static double GetAverage(int[] tab)
+{
+    double avg = 0.0;
+    foreach (int number in tab)
+    {
+        avg += number;
+    }
+    return avg/tab.Length;;
+}
